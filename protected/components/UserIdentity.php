1be95a6fb17cc32183f25847969e1f5a63eea8f8
erwinrazor <zoranrdc@yahoo.com>
2014-04-29 12:25:13 +0200

    Fixed bugs

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -19,13 +19,13 @@ class UserIdentity extends CUserIdentity
 	 */
 	public function authenticate()
 	{
-		$users=User::model()->find('LOWER(username)=?', array(strtolower($this->username)));
+		$user=User::model()->find('LOWER(username)=?', array(strtolower($this->username)));
 		
 		if ($user === null) 
 		{
 			$this->errorCode = self::ERROR_USERNAME_INVALID;
 		}
-		else if (!user->validatePassword($this->password)) 
+		else if (!$user->validatePassword($this->password)) 
 		{
 			$this->errorCode = self::ERROR_PASSWORD_INVALID;
 		}
@@ -33,19 +33,19 @@ class UserIdentity extends CUserIdentity
 		{
 			$this->_id = $user->id;
 			$this->username = $user->username;
-			$this-setState('lastLogin', date('m/d/y g:i A', strtotime($user->last_login_time)));
+			$this->setState('lastLogin', date('m/d/y g:i A', strtotime($user->last_login_time)));
 			$user->saveAttributes(array(
 				'last_login_time' => date('Y-m-d H:i:s', time()),
 			));
 			
-			$this->errorCode = self:ERROR_NONE;
+			$this->errorCode = self::ERROR_NONE;
 		}
 		
-		return $this->errorCode == self:ERROR_NONE;
+		return $this->errorCode == self::ERROR_NONE;
 	}
 	
 	public function getId() 
 	{
-		return $this->id;
+		return $this->_id;
 	}
 }
