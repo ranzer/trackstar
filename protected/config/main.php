6f0cf9ecde4ae685e81a066303c01e521c701b52
erwinrazor <zoranrdc@yahoo.com>
2014-04-30 16:30:35 +0200

    Added 'itemTable', 'itemChildTable' and 'assignmentTable' settings to 'authManager' component

--- protected/config/main.php
+++ protected/config/main.php
@@ -33,6 +33,9 @@ return array(
 		'authManager' => array(
 			'class' => 'CDbAuthManager',
 			'connectionID' => 'db',
+			'itemTable' => 'tbl_auth_item',
+			'itemChildTable' => 'tbl_auth_item_child',
+			'assignmentTable' => 'tbl_auth_assignment',
 		),
 		'user'=>array(
 			// enable cookie-based authentication
