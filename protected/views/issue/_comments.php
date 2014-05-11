59c09d4e5f207ecffefb51e68e85600aac32f9f0
Zoki <zoki@somedomain.com>
2014-05-11 23:01:39 +0200

    Fixed bug with improper call to 'encode' function

--- protected/views/issue/_comments.php
+++ protected/views/issue/_comments.php
@@ -1,7 +1,7 @@
 <?php foreach ($comments as $comment): ?>
 <div class="comment">
 	<div class="author">
-		<?php echo CHtml:encode($comment->author->username); ?>
+		<?php echo CHtml::encode($comment->author->username); ?>
 	</div>
 	<div class="time">
 		on <?php echo date('F j, Y \a\t h:i a', strtotime($comment->create_time)); ?>
