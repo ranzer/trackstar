922a42d7b4e258675bd7ce82137981911ba865a2
erwinrazor <zoranrdc@yahoo.com>
2014-05-13 12:25:08 +0200

    Included 'RecentCommentsWidget' widget in the view

--- protected/views/project/index.php
+++ protected/views/project/index.php
@@ -14,7 +14,11 @@ $this->menu=array(
 
 <h1>Projects</h1>
 
-<?php $this->widget('zii.widgets.CListView', array(
-	'dataProvider'=>$dataProvider,
-	'itemView'=>'_view',
-)); ?>
+<?php 
+	$this->widget('zii.widgets.CListView', array(
+		'dataProvider'=>$dataProvider,
+		'itemView'=>'_view',
+	)); 
+	
+	$this->widget('RecentCommentsWidget');
+?>
