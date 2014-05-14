d98913bd5e32f804441babb17032101b6d1642a1
Zoki <zoki@somedomain.com>
2014-05-14 20:42:06 +0200

    Added the name and the id of the application

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,8 +7,9 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
-
+	'name'=>'Trackstar',
+	'id'=>'Trackstar',
+	
 	// preloading 'log' component
 	'preload'=>array('log'),
 
