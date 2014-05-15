f84844fd572aafd91349bf1858b416af856bf438
Zoki <zoki@somedomain.com>
2014-05-15 21:01:00 +0200

    Added 'admin' modules to the list of modules

--- protected/config/main.php
+++ protected/config/main.php
@@ -27,7 +27,8 @@ return array(
 			'password'=>'Gii.',
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
-		)
+		),
+		'admin',
 	),
 
 	// application components
